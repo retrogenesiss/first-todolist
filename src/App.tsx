f71c3ad46034c3b97c937c4433a7428f81249e88
RetroGenesiss <106819133+RetroGenesiss@users.noreply.github.com>
2023-01-31 14:37:52 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist />
     </div>
   );
+}
+
+function Todolist() {
+  return (
+      <div>
+        <h3>What to learn</h3>
+        <div>
+          <input/>
+          <button>+</button>
+        </div>
+        <ul>
+          <li><input type="checkbox" checked={true}/><span>CSS & HTML</span></li>
+          <li><input type="checkbox" checked={true}/><span>CSS & HTML</span></li>
+          <li><input type="checkbox" checked={true}/><span>CSS & HTML</span></li>
+        </ul>
+      </div>
+  )
 }
 
 export default App;
